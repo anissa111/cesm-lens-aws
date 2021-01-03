59104bea744023e85526137d9458b474c12f7836
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-01-03 13:48:48 -0700

    initial commit

---
+++ cesm-lens-aws-site/src/components/Navbar/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavItem,
+  NavLinks,
+  NavMenu,
+} from './NavbarElements'
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">CESM LENS</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="/overview">Overview</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/data-access">Data Access</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/data">Data</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/known-issues">Known Issues</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/data-citation">Citation</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/updates">Updates</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
