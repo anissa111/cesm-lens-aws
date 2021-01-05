2a53307a25524b34c6779146a6713b6352ca971f
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-01-05 11:18:02 -0700

    Change Navbar background on scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaBars } from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib'
 import {
   Nav,
   NavbarContainer,
@@ -11,33 +12,47 @@ import {
 } from './NavbarElements'
 
 const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false)
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true)
+    } else {
+      setScrollNav(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNav)
+  }, [])
   return (
     <>
-      <Nav>
-        <NavbarContainer>
-          <NavLogo to="/">CESM LENS on AWS</NavLogo>
-          <MobileIcon onClick={toggle}>
-            <FaBars />
-          </MobileIcon>
-          <NavMenu>
-            <NavItem>
-              <NavLinks to="overview">Overview</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="data-access">Data Access</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="data">Data</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="notebooks">Notebooks</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="data-citation">Data Citation</NavLinks>
-            </NavItem>
-          </NavMenu>
-        </NavbarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <Nav scrollNav={scrollNav}>
+          <NavbarContainer>
+            <NavLogo to="/">CESM LENS on AWS</NavLogo>
+            <MobileIcon onClick={toggle}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks to="overview">Overview</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="data-access">Data Access</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="data">Data</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="notebooks">Notebooks</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="data-citation">Data Citation</NavLinks>
+              </NavItem>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   )
 }
